802de14d98bb6fb8e3c68afe781caff8835b58cc
qiang li <qxl0@yahoo.com>
2022-02-13 08:21:48 -0500

    Header: Added avatar

--- components/Avatar.js
+++ components/Avatar.js
@@ -1,10 +1,22 @@
+import Image from 'next/image'
 import React from 'react'
 import { useMoralis } from 'react-moralis'
 
 function Avatar({ username, logoutOnPress }) {
   const { user, logout } = useMoralis()
 
-  return <div>Avatar</div>
+  const imgSrc = `https://avatars.dicebear.com/api/pixel-art/${
+    username || user.get('username')
+  }.svg`
+  console.log('img src:', imgSrc)
+  return (
+    <Image
+      src={imgSrc}
+      layout="fill"
+      onClick={() => logoutOnPress && logout()}
+      className="cursor-pointer rounded-full bg-black hover:opacity-75"
+    />
+  )
 }
 
 export default Avatar
--- components/Header.js
+++ components/Header.js
@@ -1,18 +1,35 @@
 import React from 'react'
 import { useMoralis } from 'react-moralis'
 import Image from 'next/image'
+import Avatar from '../components/Avatar'
+import ChangeUsername from '../components/ChangeUsername'
+
 function Header() {
   const { user } = useMoralis()
 
   return (
-    <div>
-      <div className="relative mx-auto hidden h-24 w-24 lg:inline-grid">
-        <Image
-          src="https://links.papareact.com/3pi"
-          objectFit="cover"
-          layout="fill"
-          className="rounded-full"
-        />
+    <div className="text-pink-500">
+      <div className="">
+        <div className="relative mx-auto hidden h-24 w-24 lg:inline-grid">
+          <Image
+            src="https://links.papareact.com/3pi"
+            objectFit="cover"
+            layout="fill"
+            className="rounded-full"
+          />
+        </div>
+
+        <div>
+          <div className="relative h-48 w-48 rounded-full border-8 border-pink-500 lg:mx-auto">
+            <Avatar logoutOnPress={true} />
+          </div>
+
+          <h1 className="text-3xl">Welcome to Metaverse!!!</h1>
+
+          <h2 className="truncate text-5xl font-bold">{user.getUsername()}</h2>
+
+          <ChangeUsername />
+        </div>
       </div>
     </div>
   )
