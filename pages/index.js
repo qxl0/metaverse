0efbeb37552f39e5fba162d14d13631d87f8ebcc
qiang li <qxl0@yahoo.com>
2022-02-12 19:58:19 -0500

    Styling Home

--- pages/index.js
+++ pages/index.js
@@ -1,19 +1,26 @@
 import Head from 'next/head'
 import { useMoralis } from 'react-moralis'
 import Login from '../components/Login'
+import Header from '../components/Header'
 
 export default function Home() {
   const { isAuthenticated, logout } = useMoralis()
 
   if (!isAuthenticated) return <Login />
   return (
-    <div className="h-screen">
+    <div className="overflow-y h-screen overflow-y-scroll bg-gradient-to-b from-black to-fuchsia-900">
       <Head>
         <title>My Metaverse</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <h1>Welcome to metaverse</h1>
+      <div className="mx-auto max-w-screen-2xl">
+        {/* Header */}
+        <Header />
+
+        {/* Message */}
+      </div>
+
       <button onClick={logout}>Logout</button>
     </div>
   )
